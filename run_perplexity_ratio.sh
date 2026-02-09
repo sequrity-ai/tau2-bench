6ab840a8083e2b77f5a8587977b6186993b60c32
Zehui <Zehui@example.com>
2026-02-09 08:11:04 +0000

    add simple script

--- run_perplexity_ratio.sh
+++ run_perplexity_ratio.sh
@@ -6,15 +6,15 @@ source .venv/bin/activate
 
 # Retail
 python scripts/perplexity_ratio.py \
-  --simulation-file "data/simulations/2026-02-07T20:59:00.743831_retail_llm_agent_gpt-5-mini_user_simulator_gpt-5-mini.json" \
+  --simulation-file "data/simulations/2026-02-08T09:57:28.216107_retail_llm_agent_gpt-5-mini_user_simulator_gpt-5-mini.json" \
   --output results/ppl_retail.json
 
 # Telecom
 python scripts/perplexity_ratio.py \
-  --simulation-file "data/simulations/2026-02-07T22:32:38.484704_telecom_llm_agent_gpt-5-mini_user_simulator_gpt-5-mini.json" \
+  --simulation-file "data/simulations/2026-02-08T12:43:08.011779_telecom_llm_agent_gpt-5-mini_user_simulator_gpt-5-mini.json" \
   --output results/ppl_telecom.json
 
 # Telecom-workflow
 python scripts/perplexity_ratio.py \
-  --simulation-file "data/simulations/2026-02-08T00:08:39.797445_telecom-workflow_llm_agent_gpt-5-mini_user_simulator_gpt-5-mini.json" \
+  --simulation-file "data/simulations/2026-02-08T15:01:06.429659_telecom-workflow_llm_agent_gpt-5-mini_user_simulator_gpt-5-mini.json" \
   --output results/ppl_telecom_workflow.json
